1acc10f05de074040935053aae2a4df5c9cde7f0
tamirSa1 <tscb93@gmail.com>
2023-09-28 15:55:53 +0300

    add dist to server

--- Backend/index.ts
+++ Backend/index.ts
@@ -14,11 +14,11 @@ app.use('/users' , usersRouter)
 app.use('/vacations' , vacationsRouter)
 app.use('/followers' , followersRouter)
 
-app.use(express.static(path.join(__dirname, '../Frontend/dist')));
+app.use(express.static(path.join(__dirname, './dist')));
 
 // Serve the React app for all other routes
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../Frontend/dist', 'index.html'));
+  res.sendFile(path.join(__dirname, './dist', 'index.html'));
 });
 
 app.listen(process.env.PORT, () => {
